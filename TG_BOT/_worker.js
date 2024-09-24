66db02579edd65f417d6d1d0034486410e1c1957
njacknot <39458696+njacknot@users.noreply.github.com>
2024-09-24 17:41:06 +0800

    Update _worker.js

--- TG_BOT/_worker.js
+++ TG_BOT/_worker.js
@@ -48,9 +48,9 @@ async function handleRootRequest(request, USERNAME, PASSWORD, enableAuth) {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="Telegraph图床-基于Workers的图床服务">
-      <meta name="keywords" content="Telegraph图床,Workers图床, Cloudflare, Workers,telegra.ph, 图床">
-      <title>Telegraph图床-基于Workers的图床服务</title>
+      <meta name="description" content="TG">
+      <meta name="keywords" content="TG">
+      <title>TG-IMG</title>
       <link rel="icon" href="https://p1.meituan.net/csc/c195ee91001e783f39f41ffffbbcbd484286.ico" type="image/x-icon">
       <link href="https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/twitter-bootstrap/4.6.1/css/bootstrap.min.css" rel="stylesheet" />
       <link href="https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/bootstrap-fileinput/5.2.7/css/fileinput.min.css" rel="stylesheet" />
@@ -141,7 +141,7 @@ async function handleRootRequest(request, USERNAME, PASSWORD, enableAuth) {
     <body>
       <div class="background" id="background"></div>
       <div class="card">
-        <div class="title">Telegraph图床</div>
+        <div class="title">TG-IMG</div>
         <button type="button" class="btn" id="viewCacheBtn" title="查看历史记录"><i class="fas fa-clock"></i></button>
         <div class="card-body">
           <form id="uploadForm" action="/upload" method="post" enctype="multipart/form-data">
@@ -159,7 +159,6 @@ async function handleRootRequest(request, USERNAME, PASSWORD, enableAuth) {
             <div id="cacheContent" style="display: none;"></div>
           </form>
         </div>
-        <p class="project-link">项目开源于 GitHub - <a href="https://github.com/0-RTT/telegraph" target="_blank" rel="noopener noreferrer">0-RTT/telegraph</a></p>
         <script src="https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.6.0/jquery.min.js" type="application/javascript"></script>
         <script src="https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/bootstrap-fileinput/5.2.7/js/fileinput.min.js" type="application/javascript"></script>
         <script src="https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/bootstrap-fileinput/5.2.7/js/locales/zh.min.js" type="application/javascript"></script>
